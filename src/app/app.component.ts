0435054758cee3ddecc58adcf534503735849ace
AlbertoLyons <129765764+AlbertoLyons@users.noreply.github.com>
2024-11-30 00:19:45 -0300

    feat: added basic navbar and home

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FlowbiteService } from './services/flowbite.service';
 
 @Component({
   selector: 'app-root',
+  standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
-  title = 'Tarea2';
+export class AppComponent implements OnInit{
+  constructor(
+    private flowbiteService: FlowbiteService,
+    ) {}
+
+    ngOnInit(): void {
+      this.flowbiteService.loadFlowbite((flowbite) => {
+        console.log('Flowbite loaded', flowbite);
+      });
+    }
+    
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'portfolio',
+        pathMatch: 'full',
+        loadComponent: () => import('./portfolio/components/homepage/homepage.component').then(m => m.HomepageComponent)
+    },  
+    {
+        path:'**',
+        pathMatch: 'full',
+        redirectTo: 'portfolio'
+    }
+];
