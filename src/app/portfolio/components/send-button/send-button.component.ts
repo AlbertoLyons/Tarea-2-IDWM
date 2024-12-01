f6bd1835407804f1b649f63b43ab57a1a0535067
AlbertoLyons <alberto.lyons@alumnos.ucn.cl>
2024-12-01 15:50:27 -0300

    feat: added send button

--- src/app/portfolio/components/send-button/send-button.component.ts
+++ src/app/portfolio/components/send-button/send-button.component.ts
@@ -1,11 +1,38 @@
 import { Component } from '@angular/core';
-
+import { CommonModule } from '@angular/common';
+import { Router } from '@angular/router';
 @Component({
   selector: 'portfolio-send-button',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './send-button.component.html',
   styleUrl: './send-button.component.css'
 })
 export class SendButtonComponent {
+  buttonText: string = 'Enviar';
+  timer: number = 3;
+  togglePopup: boolean = false;
 
+  constructor(private router: Router) {}
+  sendForm() {
+    this.timer = 3;
+    this.buttonText = 'Enviando...';
+
+    const interval = setInterval(() => {
+      this.timer--;
+      if (this.timer > 0) {
+        this.buttonText = `Enviando` + '.'.repeat(this.timer);
+      } else {
+        this.buttonText = 'Enviado';
+        this.showPopup();
+        clearInterval(interval);
+      }
+    }, 1000);
+  }
+  showPopup() {
+    this.togglePopup = true;
+  }
+  closePopup() {
+    this.togglePopup = false;
+    this.router.navigate(['/portfolio']);
+  }
 }
